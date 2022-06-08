1ebad215cf878fad069b2ec3230ccd7a0e57cca1
LUKELB89 <lukelbcoding89@gmail.com>
2022-06-08 02:17:09 -0300

    changed info

--- src/pokemon.js
+++ src/pokemon.js
@@ -3,18 +3,18 @@ export const pokemon = [
 	id: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
 	name: 'Bulbasaur',
 	type: 'Grass/Poison',
-	info: 'https://www.pokemon.com/us/pokedex/bulbasaur' 
+	info: 'https://pokemon.fandom.com/wiki/Bulbasaur' 
 },
 {
 	id: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
 	name: 'Ivysaur',
 	type: 'Grass/Poison', 
-	info: 'https://www.pokemon.com/us/pokedex/ivysaur'
+	info: 'https://pokemon.fandom.com/wiki/Ivysaur'
 },
 {
 	id: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
 	name: 'Venusaur',
 	type: 'Grass/Poison',
-	info: 'https://www.pokemon.com/us/pokedex/venusaur'
+	info: 'https://pokemon.fandom.com/wiki/Venusaur'
 }]
 
